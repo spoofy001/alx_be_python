bd5221c01a69e60d3f5557dedc5cc77b056a789e
soufiane belhous <163477528+spoofy001@users.noreply.github.com>
2024-07-07 13:24:14 +0100

    Update temp_conversion_tool.py

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,5 +1,5 @@
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
-CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5  # This line was missing
 
 def convert_to_celsius(fahrenheit):
   """Converts a temperature from Fahrenheit to Celsius.
