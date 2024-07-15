538491edc9b1396c3322572d032bd130181fdf06
soufiane belhous <163477528+spoofy001@users.noreply.github.com>
2024-07-15 10:25:49 +0100

    Create basic_operations.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+addition = number2 + number1
+substraction = number1 - number2
+multiplication = number2 * number1
+print("Addition of ",number1 ,"and" ,number2, "is" ,addition)
+print("substraction of ",number1 ,"and" ,number2, "is" ,substraction)
+print("multiplication of ",number1 ,"and" ,number2, "is" ,multiplication)
