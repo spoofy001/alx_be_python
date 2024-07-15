eb55d631d62865ff2ed3c7d579f7b151a14be989
soufiane belhous <163477528+spoofy001@users.noreply.github.com>
2024-07-15 10:35:28 +0100

    Update simple_interest.py

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,5 +1,5 @@
 principal = 1000 #principal amount 
 rate = 0.05 #the annual interest rate representing 5%
 time =3  #time with a value of 3 (representing 3 years).
-interest = (principal*rate*time)
+interest = principal*rate*time
 print("The simple interest is :" , interest)
